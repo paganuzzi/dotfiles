6b966179852d8298ce72e2497149ee9ac3968275
Juan Paganuzzi <paganuzzi@gmail.com>
2023-09-22 20:56:05 -0300

    add code actions to telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,27 +1,36 @@
 return {
 	"nvim-telescope/telescope.nvim",
 	dependencies = {
+		"nvim-telescope/telescope-ui-select.nvim",
 		"nvim-lua/plenary.nvim",
 	},
-	event = "VeryLazy",
-	opts = {
-		defaults = {
-			layout_strategy = "horizontal",
-			layout_config = {
-				preview_cutoff = 10,
-				horizontal = {
-					height = 0.9,
-					preview_width = 0.65,
-					preview_cutoff = 120,
-					prompt_position = "top",
-					width = 0.95,
+	config = function()
+		require("telescope").setup({
+			extensions = {
+				["ui-select"] = {
+					require("telescope.themes").get_dropdown({}),
 				},
 			},
-			preview = {
-				treesitter = true,
+			defaults = {
+				layout_strategy = "horizontal",
+				layout_config = {
+					preview_cutoff = 10,
+					horizontal = {
+						height = 0.9,
+						preview_width = 0.65,
+						preview_cutoff = 120,
+						prompt_position = "top",
+						width = 0.95,
+					},
+				},
+				preview = {
+					treesitter = true,
+				},
 			},
-		},
-	},
+		})
+		require("telescope").load_extension("ui-select")
+	end,
+	event = "VeryLazy",
 	keys = {
 		{ "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Telescope Find Files" },
 		{ "<C-p>", "<cmd>Telescope find_files<cr>", desc = "Telescope Find Files" },
