b4b4ca06a6bf3d8bcfdbd9c3f219c08d565e106b
Juan Paganuzzi <paganuzzi@gmail.com>
2023-09-26 19:45:22 -0300

    working in cmp and toggleterm

--- lua/plugins/lspzero.lua
+++ lua/plugins/lspzero.lua
@@ -65,11 +65,31 @@ return {
 		require("lspconfig").tailwindcss.setup({})
 		require("lspconfig").tsserver.setup({})
 
+		local ELLIPSIS_CHAR = "…"
+		local MAX_LABEL_WIDTH = 40
+		local MIN_LABEL_WIDTH = 20
+
 		cmp.setup({
 			sources = {
 				{ name = "nvim_lsp" },
 				{ name = "luasnip" },
 				{ name = "buffer" },
+			},
+			view = {
+				entries = { name = "custom", selection_order = "near_cursor" },
+			},
+			formatting = {
+				format = function(_, vim_item)
+					local label = vim_item.abbr
+					local truncated_label = vim.fn.strcharpart(label, 0, MAX_LABEL_WIDTH)
+					if truncated_label ~= label then
+						vim_item.abbr = truncated_label .. ELLIPSIS_CHAR
+					elseif string.len(label) < MIN_LABEL_WIDTH then
+						local padding = string.rep(" ", MIN_LABEL_WIDTH - string.len(label))
+						vim_item.abbr = label .. padding
+					end
+					return vim_item
+				end,
 			},
 			completion = { completeopt = "menu,menuone,noinsert" },
 			mapping = cmp.mapping.preset.insert({
--- lua/plugins/terminal.lua
+++ lua/plugins/terminal.lua
@@ -3,10 +3,13 @@ return {
 	version = "*",
 	event = "VeryLazy",
 	config = function()
+		vim.keymap.set("t", "<C-w>", [[<C-\><C-n><C-w>]])
+		vim.keymap.set("t", "<Esc>", [[<C-\><C-n>]])
 		require("toggleterm").setup({
 			open_mapping = [[<F12>]],
 			insert_mappings = true,
 			terminal_mappings = true,
+			direction = "horizontal",
 		})
 	end,
 }
