8f0382aeef3dd1407fafb1f11dcf9afb076d25ea
Juan Paganuzzi <paganuzzi@gmail.com>
2023-10-04 18:34:44 -0300

    wrap to false

--- lua/config/settings.lua
+++ lua/config/settings.lua
@@ -1,17 +1,17 @@
 vim.o.number = true
 vim.o.relativenumber = true
-vim.o.mouse = 'a'
+vim.o.mouse = "a"
 vim.o.ignorecase = true
 vim.o.smartcase = true
 vim.o.hlsearch = false
-vim.o.wrap = true
+vim.o.wrap = false
 vim.o.breakindent = true
 vim.o.tabstop = 2
 vim.o.shiftwidth = 2
 vim.o.expandtab = false
 vim.o.showmode = false
 vim.o.termguicolors = true
-vim.o.clipboard = 'unnamedplus'
+vim.o.clipboard = "unnamedplus"
 vim.o.undofile = true
 
 -- Case-insensitive searching UNLESS \C or capital in search
